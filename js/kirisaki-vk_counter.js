636b0687f8c1c6a7b167471f23f0a57d346df85e
Kirisaki_vk <85801331+kirisaki-vk@users.noreply.github.com>
2023-02-01 13:47:08 +0300

    Removed unused css/js

--- js/kirisaki-vk_counter.js
+++
@@ -1,9 +0,0 @@
-const el = document.querySelector( '.counter' )
-new Waypoint( {
-    element: el,
-    handler: function() { 
-        counterUp( el ) 
-        this.destroy()
-    },
-    offset: 'bottom-in-view',
-} )
